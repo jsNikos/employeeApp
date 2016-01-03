c681ed5120528039a223e13498e561d98f95de59
nikos <nikos@tiger>
2016-01-03 12:55:19 +0200

    message creation view

--- public/messages/messagesModule.js
+++ public/messages/messagesModule.js
@@ -1,6 +1,48 @@
 angular.module('messagesModule', [])
   .controller('MessagesController', ['$scope', '$http',
     function($scope, $http) {
+      $scope.roles = undefined;
+      $scope.selectedMessage = undefined;
+      $scope.newMessage = undefined;
+      $scope.availableReceipients = undefined;
+
+      $scope.showView = undefined; // editor
+
       $scope.$emit('initialized', 'messages');
+
+      $scope.handleSelectMessage = function(message) {
+        $scope.selectedMessage = message;
+      };
+
+      $scope.handleShowMessageCreator = function() {
+        $scope.newMessage = {
+          content: {
+            title: null,
+            body: null
+          },
+          to: undefined
+        };
+        $http.get('/employees/api/findEmployees')
+          .then(function(resp) {
+            $scope.availableReceipients = resp.data;
+          })
+          .catch(console.log);
+        $scope.showView = 'editor';
+      };
+
+      $scope.handleSendMessage = function(message){
+        $http.put('/message/api/create', message)
+          .then(function(resp) {
+            $scope.showView = undefined;
+            $scope.newMessage = undefined;
+          })
+          .catch(console.log);
+      }
+
+      $scope.handleCancelCreate = function() {
+        $scope.showView = undefined;
+        $scope.newMessage = undefined;
+      }
+
     }
   ]);
